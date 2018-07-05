42a940d94048875c97b969dc22bdbfb747fa4e41
Anthony Cortez <anthony1154@Anthony-Cortezs-MacBook-Pro.local>
2018-07-04 20:41:44 -0700

    Prod

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ mongoose.connect(MONGODB_URI);
 app.use(express.static('client/build'));
 
 //const path = require('path');
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 })
 
