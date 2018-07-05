4af16c0284c954239df913745a4ca31747ced5c2
Anthony Cortez <anthony1154@Anthony-Cortezs-MacBook-Pro.local>
2018-07-04 20:36:22 -0700

    Prod

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ var PORT = process.env.PORT || 5000;
 
 var app = express();
 
-var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/NewYorkTimes'
-
 const path = require('path');
+
+var cors = require('cors');
+
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/NewYorkTimes'
 
 app.use(logger("dev"));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
