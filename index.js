aac946172f31398aa2c99e0127b6b311495aaa6d
Anthony Cortez <anthony1154@Anthony-Cortezs-MacBook-Pro.local>
2018-07-04 20:05:03 -0700

    Prod

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ mongoose.connect(MONGODB_URI);
 //     res.send('Hello There')
 // })
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
- 
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
-}
+
+app.use(express.static('client/build'));
+
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+})
+
 
 app.get('/api/articles', function (req,res) {
    db.Article.find({}).then(function(dbArticle) {
